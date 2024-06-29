5f40a479f4fa6d14b707ac58a6534713ec9bd497
RAJAT DUGAR <rajat02514@gmail.com>
2024-06-29 13:54:28 +0530

    chat part almost done not tested tho and instead of send jwt token set it in cookie

--- src/server.ts
+++ src/server.ts
@@ -1,120 +1,115 @@
-import express from "express"
-import bcrypt from "bcrypt"
-import cors from "cors"
-import bodyParser from "body-parser"
-import jwt from "jsonwebtoken"
+import express, { Request, Response, NextFunction } from "express";
+import bcrypt from "bcrypt";
+import cors from "cors";
+import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
-import authMiddleware from "./middlewares/authmiddleware"
-import dotenv from "dotenv"
-
-const PORT: number = 3001;
+import authMiddleware from "./middlewares/authmiddleware";
+import dotenv from "dotenv";
+import cookie from "cookie";
+const WebSocket = require("ws");
+const PORT: number = 3002;
 const SECRET_KEY: string = "webops2024"; //for signing the jwt token
 const app = express();
 const prisma = new PrismaClient();
-dotenv.config()
+dotenv.config();
 
 console.log("Connected to the DB");
 
-//define the interfaces
+// Define the interfaces
 type User = {
-  id:number,
-  username:string,
-  rollNo:string,
-  email:string,
-  password:string
-}
-interface Product 
-{
-  name: string,
-  description: string,
-  category: string,
-  costPrice: number,
-  sellingPrice: number,
-  image: string,
-  userId: number,
-  
-  condition: string
+  id: number;
+  username: string;
+  rollNo: string;
+  email: string;
+  password: string;
+};
+
+interface Product {
+  name: string;
+  description: string;
+  category: string;
+  costPrice: number;
+  sellingPrice: number;
+  image: string;
+  userId: number;
+  condition: string;
 }
 
+interface Message {
+  senderId: number;
+  roomName: string;
+  text: string;
+}
 
-//add the necessary middlewares
+// Add the necessary middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
-//index route
-app.get("/", (req: any, res: any) => {
+// Index route
+app.get("/", (req: Request, res: Response) => {
   res.send("Server running");
 });
-  
-//auth routes
-app.post("/signup", (req: any, res: any) => {
+
+// Auth routes
+app.post("/signup", async (req: Request, res: Response) => {
   const { username, password, email, rollNo } = req.body;
   const saltRounds: number = 10;
 
   console.log(username, password, email);
 
-  //hashing the password using bcrypt js
-
-  bcrypt
-    .hash(password, saltRounds)
-    .then(async (hashedPassword: string) => {
-      //create the user here.
-      console.log("Password hashed successfully.");
-      const user = await prisma.user.create({
-        data: {
-          username: username,
-          password: hashedPassword,
-          email: email,
-          rollNo:rollNo
-        },
-      });
-      if (user) {
-        const token: string = jwt.sign({userId: user.id}, SECRET_KEY);
-        res.status(200).json({ message: "User created successfully." , token:token});
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res
-        .status(500)
-        .json({ message: "Server error: "+err }); //this here would basically return an error if the username is already taken. or for any other error, the error will be sent to the frontend.
-        
+  // Hashing the password using bcrypt
+  try {
+    const hashedPassword: string = await bcrypt.hash(password, saltRounds);
+    console.log("Password hashed successfully.");
+    const user = await prisma.user.create({
+      data: {
+        username: username,
+        password: hashedPassword,
+        email: email,
+        rollNo: rollNo,
+      },
     });
+    if (user) {
+      const token: string = jwt.sign({ userId: user.id }, SECRET_KEY);
+      res
+        .status(200)
+        .json({ message: "User created successfully.", token: token });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error: " + err });
+  }
 });
 
-app.post("/signin", async (req: any, res: any) => {
+app.post("/signin", async (req: Request, res: Response) => {
   const { rollNo, password } = req.body;
 
-  //check if the user exists on the DB,if exists retreive it and compare the password
   const user: User | null = await prisma.user.findUnique({
     where: {
       rollNo: rollNo,
     },
   });
+
   if (user) {
-    //if user exists, log him in
-    const hashedPassword = user.password;
-    bcrypt
-      .compare(password, hashedPassword) //hashedPassword will be retreived from the database.
-      .then((result: boolean) => {
-        if (result) {
-          //now send the token to the client and make it store in the localstorage.
-          const token: string = jwt.sign({userId: user.id}, SECRET_KEY);
-          res.status(200).json({ token: token, message: "Login successful" });
-        } else {
-          res.status(401).json({ message: "Invalid credentials" });
-        }
-      })
-      .catch((err ) => {
-        res.status(500).json({ message: "Internal server error" });
-      });
+    try {
+      const result: boolean = await bcrypt.compare(password, user.password);
+      if (result) {
+        const token: string = jwt.sign({ userId: user.id }, SECRET_KEY);
+        res.status(200).json({ token: token, message: "Login successful" });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    } catch (err) {
+      res.status(500).json({ message: "Internal server error" });
+    }
   } else {
     res.status(401).json({ message: "No user found with the given username." });
   }
 });
 
-// user-info routes
-app.get("/user/:id",authMiddleware, async (req: any, res: any) => {
+// User-info routes
+app.get("/user/:id", authMiddleware, async (req: any, res: Response) => {
   const { id } = req.params;
   const user: User | null = await prisma.user.findUnique({
     where: {
@@ -126,15 +121,13 @@ app.get("/user/:id",authMiddleware, async (req: any, res: any) => {
   } else {
     res.status(404).json({ message: "No user found with the given id." });
   }
-}
-);
-
-//details for the logged in user.
-app.get("/user",authMiddleware, async (req: any, res: any) => {
-  const  id = req.userId;
+});
+console.log("hello 1");
+app.get("/user", authMiddleware, async (req: any, res: Response) => {
+  const id = req.userId;
   const user: User | null = await prisma.user.findUnique({
     where: {
-      id: (id),
+      id: id,
     },
   });
   if (user) {
@@ -142,122 +135,173 @@ app.get("/user",authMiddleware, async (req: any, res: any) => {
   } else {
     res.status(404).json({ message: "No user found with the given id." });
   }
-}
-);
- 
-//delete all users
-app.delete("/users",async (req,res)=>{
-  await prisma.user.deleteMany().then(()=>{return res.json({message:"deleted all users."})})
-})
+});
 
-//product routes
-app.post("/product/add",authMiddleware,async (req: any,res: any)=>{
-  //we need to add a auth middleware here to check if the user is authenticated or not.
-  
-  //here the data about the product that we are getting must be in sync with the prisma schema so that we don't need to desructure it.
+// Delete all users
+app.delete("/users", async (req: Request, res: Response) => {
+  await prisma.user.deleteMany();
+  res.json({ message: "deleted all users." });
+});
+
+// Product routes
+app.post("/product/add", authMiddleware, async (req: any, res: Response) => {
   const productData = req.body;
-  console.log(productData.name)
-  
-  //now create the product in the database
-  try 
-  {
+
+  try {
     const product: Product = {
       name: productData.name,
       description: productData.description,
-      
-      image:productData.image,
-      userId: req.userId, //this is the user id that we are getting from the auth middleware.
-      category:productData.category,
+      image: productData.image,
+      userId: req.userId,
+      category: productData.category,
       costPrice: productData.costPrice,
       sellingPrice: productData.sellingPrice,
-      condition: productData.condition
-    }
-    const productAdded = await prisma.product.create({
-      data:product
-    })
-    res.status(200).json({message:"Product added successfully."});
+      condition: productData.condition,
+    };
+    await prisma.product.create({
+      data: product,
+    });
+    res.status(200).json({ message: "Product added successfully." });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Error adding the product, please try again." });
   }
-  catch(err)
-  {
-      console.log(err)
-      res.status(500).json({message:"Error adding the product, please try again."})
-  }
-  
+});
 
-})
-
-app.get("/products",async (req:any, res: any)=>{
-  // get all the products requesting to this route.
-  try 
-  {
+app.get("/products", async (req: Request, res: Response) => {
+  try {
     const products = await prisma.product.findMany({
-      include:{
-        user:true
-      }
-    })
-    res.status(200).json({message:"Retrieval successfull!",products:products})
-  }
-  catch(err)
-  {
-    console.log(err)
-    res.status(500).json({message:"Internal server error."})
-  }
-  
-  
-})
-
-//delete all products
-app.delete("/products",async (req:any, res: any)=>{
-  // get all the products requesting to this route.
-  try 
-  {
-    const products = await prisma.product.deleteMany({
-     
-    })
-    res.status(200).json({message:"deleted! successfull!",products:products})
-  }
-  catch(err)
-  {
-    console.log(err)
-    res.status(500).json({message:"Internal server error."})
-  }
-  
-  
-})
-
-app.get("/product/:id", async (req:any, res:any)=>{
-  const {id} = req.params;
-  try 
-  {
-    const product: Product | null = await prisma.product.findUnique({
-      where:{
-        productId: parseInt(id)
+      include: {
+        user: true,
       },
-      include:{
-        user:true
-      }
-    })
-    if (product)
-    {
-      
-      res.status(200).json({message:"Product found.",product:product})
-    }
-    else 
-    {
-      res.status(200).json({message:"No product found with the given id."})
-    }
+    });
+    res
+      .status(200)
+      .json({ message: "Retrieval successful!", products: products });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error." });
   }
-  catch(err)
-  {
-    console.log(err)
-    res.status(500).json({message:"Internal server error."})
+});
+
+// Delete all products
+app.delete("/products", async (req: Request, res: Response) => {
+  try {
+    await prisma.product.deleteMany({});
+    res.status(200).json({ message: "Deleted successfully!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error." });
   }
-})
+});
 
-//chat logic for people.
+app.get("/product/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const product: Product | null = await prisma.product.findUnique({
+      where: {
+        productId: parseInt(id),
+      },
+      include: {
+        user: true,
+      },
+    });
+    if (product) {
+      res.status(200).json({ message: "Product found.", product: product });
+    } else {
+      res.status(200).json({ message: "No product found with the given id." });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
 
+// Chat logic for messages
+app.get("/messages/:roomName", async (req: Request, res: Response) => {
+  const { roomName } = req.params;
+  try {
+    const messages: Message[] = await prisma.message.findMany({
+      where: {
+        roomName: roomName,
+      },
+    });
+    res.json({ messages: messages });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
- 
+const wss = new WebSocket.Server({ server });
+interface ChatRooms {
+  [roomName: string]: WebSocket[];
+}
+const chatRooms: ChatRooms = {};
+
+wss.on("connection", (ws: WebSocket, req: Request) => {
+  const cookies = req.headers.cookie ? cookie.parse(req.headers.cookie) : {};
+  const token = cookies.token;
+
+  if (!token) {
+    ws.close();
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY) as { userId: number };
+    (ws as any).userId = decoded.userId;
+  } catch (error) {
+    ws.close();
+  }
+
+  ws.onmessage = async (event: MessageEvent) => {
+    const message: Message = JSON.parse(event.data.toString());
+    const { roomName, text } = message;
+
+    if (!chatRooms[roomName]) {
+      chatRooms[roomName] = [];
+    }
+
+    chatRooms[roomName].push(ws);
+
+    console.log(
+      `Received message from user ${
+        (ws as any).userId
+      } in room ${roomName}: ${text}`
+    );
+
+    // Broadcast message to all clients in the same room
+    const createmsg = await prisma.message.create({
+      data: {
+        text,
+        senderId: (ws as any).userId,
+        roomName,
+      },
+    });
+    chatRooms[roomName].forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(
+          JSON.stringify({ senderId: (ws as any).userId, message: text })
+        );
+      }
+    });
+  };
+
+  ws.onclose = () => {
+    console.log(`Connection closed for user ${(ws as any).userId}`);
+    Object.keys(chatRooms).forEach((roomName) => {
+      chatRooms[roomName] = chatRooms[roomName].filter(
+        (client) => client !== ws
+      );
+      if (chatRooms[roomName].length === 0) {
+        delete chatRooms[roomName];
+      }
+    });
+  };
+});
